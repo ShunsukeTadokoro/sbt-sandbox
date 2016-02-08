dff53278b3b0bea97559ca337641195ca2a6abd1
田所 駿佑 <shunsuke.tadokoro@bizreach.co.jp>
2016-02-08 13:31:47 +0900

    initial commit

---
+++ build.sbt
@@ -0,0 +1,12 @@
+name := "sbt-sandbox"
+
+version := "1.0"
+
+scalaVersion := "2.11.7"
+
+libraryDependencies ++= Seq(
+  "org.scalaz" %% "scalaz-core" % "7.2.0",
+  "org.scalatest" %% "scalatest" % "2.2.6" % "test"
+)
+
+    
---
+++ src/main/scala/io/ShunsukeTadokoro/SimpleHttpServer/ErrorMessages.scala
@@ -0,0 +1,9 @@
+package io.ShunsukeTadokoro.SimpleHttpServer
+
+/**
+ * @author Shunsuke Tadokoro
+ */
+object ErrorMessages {
+  val NotFound = "<h1>Not Found</h1><p>お探しのページは見つかりません</p>"
+  val InternalServerError = "<h1>Internal Server Error</h1><p>なにかおかしい</p>"
+}
